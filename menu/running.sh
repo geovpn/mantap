145b47f12d39606358c86eb9ed70f1190e3d63d7
Muhammad Amin <96859296+geovpn@users.noreply.github.com>
2022-01-24 22:34:29 +0800

    Update running.sh

--- menu/running.sh
+++ menu/running.sh
@@ -1,6 +1,87 @@
 #!/bin/bash
 # IP Validation
-MYIP=$(wget -qO- icanhazip.com);
+# Getting
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+#########################
+
+BURIQ () {
+    curl -sS https://raw.githubusercontent.com/geovpn/perizinan/main/main/allow > /root/tmp
+    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
+    for user in "${data[@]}"
+    do
+    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
+    d1=(`date -d "$exp" +%s`)
+    d2=(`date -d "$biji" +%s`)
+    exp2=$(( (d1 - d2) / 86400 ))
+    if [[ "$exp2" -le "0" ]]; then
+    echo $user > /etc/.$user.ini
+    else
+    rm -f /etc/.$user.ini > /dev/null 2>&1
+    fi
+    done
+    rm -f /root/tmp
+}
+
+MYIP=$(curl -sS ipinfo.io/ip)
+Name=$(curl -sS https://raw.githubusercontent.com/geovpn/perizinan/main/main/allow | grep $MYIP | awk '{print $2}')
+echo $Name > /usr/local/etc/.$Name.ini
+CekOne=$(cat /usr/local/etc/.$Name.ini)
+
+Bloman () {
+if [ -f "/etc/.$Name.ini" ]; then
+CekTwo=$(cat /etc/.$Name.ini)
+    if [ "$CekOne" = "$CekTwo" ]; then
+        res="Expired"
+    fi
+else
+res="Permission Accepted..."
+fi
+}
+
+PERMISSION () {
+    MYIP=$(curl -sS ipinfo.io/ip)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/geovpn/perizinan/main/main/allow | awk '{print $4}' | grep $MYIP)
+    if [ "$MYIP" = "$IZIN" ]; then
+    Bloman
+    else
+    res="Permission Denied!"
+    fi
+    BURIQ
+}
+red='\e[1;31m'
+green='\e[1;32m'
+NC='\e[0m'
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+PERMISSION
+
+if [ -f /home/needupdate ]; then
+red "Your script need to update first !"
+exit 0
+elif [ "$res" = "Permission Accepted..." ]; then
+echo -ne
+else
+red "Permission Denied!"
+exit 0
+fi
+clear
+PERMISSION
+
+if [ "$res" = "Expired" ]; then
+Exp="\033[1;31mExpired\033[0m"
+rm -f /home/needupdate > /dev/null 2>&1
+else
+Exp=$(curl -sS https://raw.githubusercontent.com/geovpn/perizinan/main/main/allow | grep $MYIP | awk '{print $3}')
+fi
+chck_b(){
+	PID=`ps -ef |grep -v grep | grep scvps_bot |awk '{print $2}'`
+	if [[ ! -z "${PID}" ]]; then
+			sts="\033[0;32m◉ \033[0m"
+		else
+			sts="\033[1;31m○ \033[0m"
+    fi
+}
 
 # VPS Information
 Checkstart1=$(ip route | grep default | cut -d ' ' -f 3 | head -n 1);
@@ -383,9 +464,9 @@ echo -e "Public IP   : $MYIP"
 echo -e "Domain      : $Domen"
 echo -e "ISP Name    : $NAMAISP"
 echo -e "Region      : $REGION "
-echo -e "Country     : $COUNTRY"
-echo -e "Date        : $harini"
-echo -e "Time        : $jam ( WIB )"
+echo -e "Client Name : $Name"
+echo -e "Exp Script  : $Exp"
+echo -e "Version     : Latest Version"
 echo "----------------------------------------------------------------" | lolcat 
 echo "SYSTEM STATUS INFORMATION :" | lolcat 
 echo -e "SSH / Tun          : $status_ssh"
