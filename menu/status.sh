fce3b874c090be0f06628c4bf8264430ce2b928d
Muhammad Amin <96859296+geovpn@users.noreply.github.com>
2022-01-24 21:52:18 +0800

    Update status.sh

--- menu/status.sh
+++ menu/status.sh
@@ -18,16 +18,6 @@ bd='\e[1m'
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/geovpn/perizinan/main/ip | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${NC}${GREEN}Permission Accepted...${NC}"
-else
-echo -e "${NC}${RED}Permission Denied!${NC}";
-echo -e "${NC}${LIGHT}Please Contact Admin!!"
-echo -e "${NC}${LIGHT}Telegram : https://t.me/sampiiiiu"
-exit 0
-fi
-clear
 AKTIF="$bd Aktif [ ${green}Running${NC} ]"
 ERROR="Error [ ${red}Not Running${NC} ]"
 
