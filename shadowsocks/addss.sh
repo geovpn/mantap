121092faf55d520b084e04a7ddfd41e5098a7e4f
Muhammad Amin <96859296+geovpn@users.noreply.github.com>
2022-01-25 07:02:21 +0800

    Update addss.sh

--- shadowsocks/addss.sh
+++ shadowsocks/addss.sh
@@ -77,7 +77,7 @@ red "Permission Denied!"
 exit 0
 fi
 clear
-IP=$(wget -qO- ipinfo.io/ip);
+MYIP=$(curl -sS ipinfo.io/ip)
 lastport1=$(grep "port_tls" /etc/shadowsocks-libev/akun.conf | tail -n1 | awk '{print $2}')
 lastport2=$(grep "port_http" /etc/shadowsocks-libev/akun.conf | tail -n1 | awk '{print $2}')
 if [[ $lastport1 == '' ]]; then
