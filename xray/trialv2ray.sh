3c73479b46a55f5dc8c84d252b6fb42d946c686d
Muhammad Amin <96859296+geovpn@users.noreply.github.com>
2022-01-24 19:17:46 +0800

    Update trialv2ray.sh

--- xray/trialv2ray.sh
+++ xray/trialv2ray.sh
@@ -10,81 +10,16 @@ white='\x1b[37m'
 bold='\033[1m'
 off='\x1b[m'
 flag='\x1b[47;41m'
-# Getting
-MYIP=$(wget -qO- ipinfo.io/ip);
+MYIP=$(wget -qO- ipv4.icanhazip.com);
 echo "Checking VPS"
-# Getting
-dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-#########################
-
-BURIQ () {
-    curl -sS https://raw.githubusercontent.com/geovpn/perizinan/main/main/allow > /root/tmp
-    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
-    for user in "${data[@]}"
-    do
-    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
-    d1=(`date -d "$exp" +%s`)
-    d2=(`date -d "$biji" +%s`)
-    exp2=$(( (d1 - d2) / 86400 ))
-    if [[ "$exp2" -le "0" ]]; then
-    echo $user > /etc/.$user.ini
-    else
-    rm -f /etc/.$user.ini > /dev/null 2>&1
-    fi
-    done
-    rm -f /root/tmp
-}
-
-MYIP=$(curl -sS ipinfo.io/ip)
-Name=$(curl -sS https://raw.githubusercontent.com/geovpn/perizinan/main/main/allow | grep $MYIP | awk '{print $2}')
-echo $Name > /usr/local/etc/.$Name.ini
-CekOne=$(cat /usr/local/etc/.$Name.ini)
-
-Bloman () {
-if [ -f "/etc/.$Name.ini" ]; then
-CekTwo=$(cat /etc/.$Name.ini)
-    if [ "$CekOne" = "$CekTwo" ]; then
-        res="Expired"
-    fi
+IZIN=$( curl https://raw.githubusercontent.com/geovpn/perizinan/main/ip | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${green}Permission Accepted...${NC}"
 else
-res="Permission Accepted..."
-fi
-}
-
-PERMISSION () {
-    MYIP=$(curl -sS ipinfo.io/ip)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/geovpn/perizinan/main/main/allow | awk '{print $4}' | grep $MYIP)
-    if [ "$MYIP" = "$IZIN" ]; then
-    Bloman
-    else
-    res="Permission Denied!"
-    fi
-    BURIQ
-}
-red='\e[1;31m'
-green='\e[1;32m'
-NC='\e[0m'
-green() { echo -e "\\033[32;1m${*}\\033[0m"; }
-red() { echo -e "\\033[31;1m${*}\\033[0m"; }
-PERMISSION
-
-if [ -f /home/needupdate ]; then
-red "Your script need to update first !"
-exit 0
-elif [ "$res" = "Permission Accepted..." ]; then
-echo -ne
-else
-red "Permission Denied!"
+echo -e "${red}Permission Denied!${NC}";
+echo "Only For Premium Users"
 exit 0
 fi
-clear
-
-ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
-CITY=$(curl -s ipinfo.io/city )
-COUNTRY=$(curl -s ipinfo.io/country )
-
-MYIP=$(wget -qO- ipinfo.io/ip);
 clear
 source /var/lib/geovpnstore/ipvps.conf
 if [[ "$IP" = "" ]]; then
@@ -95,38 +30,42 @@ fi
 read -rp "Bug: " -e bug
 tls="$(cat ~/log-install.txt | grep -w "Vmess TLS" | cut -d: -f2|sed 's/ //g')"
 nontls="$(cat ~/log-install.txt | grep -w "Vmess None TLS" | cut -d: -f2|sed 's/ //g')"
-user=Trial-`</dev/urandom tr -dc X-Z0-9 | head -c2`
+
+# Create Expried 
+masaaktif="1"
+exp=$(date -d "$masaaktif days" +"%Y-%m-%d")
+
+# Make Random Username 
+user=Trial`</dev/urandom tr -dc X-Z0-9 | head -c4`
 uuid=$(cat /proc/sys/kernel/random/uuid)
-tnggl=$(date +"%R")
-read -p "Expired (Jam): " ktf
-exp=`date -d "$ktf hour" +"%R"`
-sed -i '/#xray-v2ray-tls$/a\### '"$user $exp"'\
-},{"id": "'""$uuid""'","alterId": '"0"',"email": "'""$user""'"' /etc/xray/v2ray-tls.json
-sed -i '/#xray-v2ray-nontls$/a\### '"$user $exp"'\
-},{"id": "'""$uuid""'","alterId": '"0"',"email": "'""$user""'"' /etc/xray/v2ray-nontls.json
-cat>/etc/xray/v2ray-$user-tls.json<<EOF
+created=`date -d "0 days" +"%d-%m-%Y"`
+sed -i '/#tls$/a\### '"$user $exp"'\
+},{"id": "'""$uuid""'","alterId": '"2"',"email": "'""$user""'"' /etc/v2ray/config.json
+sed -i '/#none$/a\### '"$user $exp"'\
+},{"id": "'""$uuid""'","alterId": '"2"',"email": "'""$user""'"' /etc/v2ray/none.json
+cat>/etc/xray/$user-tls.json<<EOF
       {
       "v": "2",
       "ps": "${user}",
       "add": "${domain}",
-      "port": "${nontls}",
+      "port": "${tls}",
       "id": "${uuid}",
-      "aid": "0",
+      "aid": "2",
       "net": "ws",
       "path": "geo",
       "type": "none",
-      "host": "",
-      "tls": "none"
+      "host": "${domain}",
+      "tls": "tls"
 }
 EOF
-cat>/etc/xray/v2ray-$user-nontls.json<<EOF
+cat>/etc/xray/$user-none.json<<EOF
       {
       "v": "2",
       "ps": "${user}",
       "add": "${bug}",
-      "port": "${nontls}",
+      "port": "${none}",
       "id": "${uuid}",
-      "aid": "0",
+      "aid": "2",
       "net": "ws",
       "path": "geo",
       "type": "none",
@@ -136,31 +75,32 @@ cat>/etc/xray/v2ray-$user-nontls.json<<EOF
 EOF
 vmess_base641=$( base64 -w 0 <<< $vmess_json1)
 vmess_base642=$( base64 -w 0 <<< $vmess_json2)
-xrayv2ray1="vmess://$(base64 -w 0 /etc/xray/v2ray-$user-tls.json)"
-xrayv2ray2="vmess://$(base64 -w 0 /etc/xray/v2ray-$user-nontls.json)"
-systemctl restart xray@v2ray-tls
-systemctl restart xray@v2ray-nontls
+vmesslink1="vmess://$(base64 -w 0 /etc/xray/$user-tls.json)"
+vmesslink2="vmess://$(base64 -w 0 /etc/xray/$user-none.json)"
+systemctl restart xray
+systemctl restart xray@none
 service cron restart
 clear
-echo -e ""
-echo -e "${cyan}==================================${off}"
-echo -e "${purple} ~> TRIAL V2RAY VMESS${off}"
-echo -e "${cyan}==================================${off}"
+echo -e "${cyan}=====================${off}"
+echo -e "${cyan}TRIAL XRAY /VMESS${off}"
+echo -e "${cyan}=====================${off}"
 echo -e " Remarks        : ${user}"
 echo -e " Bug            : ${bug}"
 echo -e " Domain         : ${domain}"
 echo -e " Port TLS       : ${tls}"
 echo -e " Port No TLS    : ${nontls}"
 echo -e " ID             : ${uuid}"
-echo -e " AlterID        : 0"
+echo -e " AlterID        : 2"
 echo -e " Security       : auto"
 echo -e " Network        : ws"
 echo -e " Path           : geo${off}"
-echo -e "${cyan}==================================${off}"
+echo -e "${cyan}=====================${off}"
 echo -e "${purple}~> VMESS TLS : $off${xrayv2ray1}"
-echo -e "${cyan}==================================${off}"
+echo -e "${cyan}=====================${off}"
 echo -e "${purple}~> VMESS NON-TLS : $off${xrayv2ray2}"
-echo -e "${cyan}==================================${off}"
-echo -e " ${green}Aktif Selama   : $ktf Jam"
-echo -e "${cyan}==================================${off}"
+echo -e "${cyan}=====================${off}"
+echo -e " ${green}Aktif Selama   : $masaaktif Hari"
+echo -e "${cyan}=====================${off}"
+echo -e ""
+echo -e "Script By Geo.NTB" | lolcat
 echo -e ""
