4c5e68f2c15eb61b89ce1392c296ed8464f5216d
Muhammad Amin <96859296+geovpn@users.noreply.github.com>
2022-01-24 16:59:30 +0800

    Add files via upload

--- trial/trialtrgo.sh
+++ trial/trialtrgo.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 grey='\x1b[90m'
 red='\x1b[91m'
 green='\x1b[92m'
@@ -17,9 +18,8 @@ COUNTRY=$(curl -s ipinfo.io/country )
 
 MYIP=$(wget -qO- ipinfo.io/ip);
 clear
-uuid=$(cat /etc/trojan-go/uuid.txt)
-clear
-uuid=$(cat /etc/trojan-go/uuid.txt)
+uuid1=$(cat /etc/trojan-go/uuid.txt)
+uuid2=$(cat /etc/trojan/uuid.txt)
 source /var/lib/geovpnstore/ipvps.conf
 if [[ "$IP" = "" ]]; then
 domain=$(cat /etc/xray/domain)
@@ -34,13 +34,15 @@ exp=$(date -d "$masaaktif days" +"%Y-%m-%d")
 # Make Random Username 
 user=Trial`</dev/urandom tr -dc X-Z0-9 | head -c4`
 users="Trojan-GO_$user"
-sed -i '/"'""$uuid""'"$/a\,"'""$user""'"' /etc/trojan-go/config.json
+user2="Trojan-GFW_$user"
+sed -i '/"'""$uuid1""'"$/a\,"'""$users""'"' /etc/trojan-go/config.json
+sed -i '/"'""$uuid2""'"$/a\,"'""$user2""'"' /etc/trojan/config.json
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
 echo -e "### $user $exp" >> /etc/trojan-go/akun.conf
 echo -e "### $user $exp" >> /etc/trojan/akun.conf
 systemctl restart trojan-go
 systemctl restart trojan
-link="trojan-go://${user}@${domain}:${trgo}/?sni=${domain}&type=ws&host=${domain}&path=/trojango&encryption=none#$user"
+trojangolink="trojan-go://${users}@${domain}:${trojango}/?sni=${domain}&type=ws&host=${domain}&path=/trojango&encryption=none#${user}"
 cat > client.json << END
 {
     "run_type": "client",
@@ -64,20 +66,23 @@ cat > client.json << END
     }
 }
 END
+mv client.json /home/vps/public_html/${user}-IgniterGO.json
 clear
 echo -e ""
 echo -e "${red}=================================${off}"
-echo -e "${blue} ~>    TRIAL TROJAN-GOVPN${off}"
+echo -e "${blue} ~> TRIAL TROJAN-GO${off}"
 echo -e "${red}=================================${off}"
-echo -e "Remarks    : ${user}"
-echo -e "IP/Host    : ${MYIP}"
-echo -e "Address    : ${domain}"
-echo -e "Port       : ${trgo}"
-echo -e "Key        : ${user}"
-echo -e "Encryption : none"
-echo -e "Path       : /trojango"
+echo -e " ISP              : ${ISP}"
+echo -e " CITY             : ${CITY}"
+echo -e " COUNTRY          : ${COUNTRY}"
+echo -e " Remarks          : ${user}"
+echo -e " Host             : ${domain}"
+echo -e " Port Trojan-GO   : ${trgo}"
+echo -e " Path WebSocket   : /trojango"
 echo -e "${red}=================================${off}"
-echo -e "Link TrGo  : ${link}"
+echo -e " Trojan-GO        : ${trojangolink}"
+echo -e "${red}=================================${off}"
+echo -e " Igniter-GO       : http://${domain}:85/${user}-IgniterGO.json"
 echo -e "${red}=================================${off}"
 echo -e " Aktif Selama     : $masaaktif Hari"
 echo -e "${red}=================================${off}"
--- trial/trialv2ray.sh
+++ trial/trialv2ray.sh
@@ -12,14 +12,6 @@ off='\x1b[m'
 flag='\x1b[47;41m'
 MYIP=$(wget -qO- ipv4.icanhazip.com);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/geovpn/perizinan/main/ip | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${green}Permission Accepted...${NC}"
-else
-echo -e "${red}Permission Denied!${NC}";
-echo "Only For Premium Users"
-exit 0
-fi
 clear
 source /var/lib/geovpnstore/ipvps.conf
 if [[ "$IP" = "" ]]; then
@@ -31,40 +23,37 @@ fi
 masaaktif="1"
 exp=$(date -d "$masaaktif days" +"%Y-%m-%d")
 
-read -rp "Bug: " -e bug
-tls="$(cat ~/log-install.txt | grep -w "Vmess TLS" | cut -d: -f2|sed 's/ //g')"
-nontls="$(cat ~/log-install.txt | grep -w "Vmess None TLS" | cut -d: -f2|sed 's/ //g')"
 # Make Random Username 
 user=Trial`</dev/urandom tr -dc X-Z0-9 | head -c4`
 uuid=$(cat /proc/sys/kernel/random/uuid)
 created=`date -d "0 days" +"%d-%m-%Y"`
-sed -i '/#xray-v2ray-tls$/a\### '"$user $exp"'\
-},{"id": "'""$uuid""'","alterId": '"0"',"email": "'""$user""'"' /etc/xray/v2ray-tls.json
-sed -i '/#xray-v2ray-nontls$/a\### '"$user $exp"'\
-},{"id": "'""$uuid""'","alterId": '"0"',"email": "'""$user""'"' /etc/xray/v2ray-nontls.json
-cat>/etc/xray/v2ray-$user-tls.json<<EOF
+sed -i '/#tls$/a\### '"$user $exp"'\
+},{"id": "'""$uuid""'","alterId": '"2"',"email": "'""$user""'"' /etc/v2ray/config.json
+sed -i '/#none$/a\### '"$user $exp"'\
+},{"id": "'""$uuid""'","alterId": '"2"',"email": "'""$user""'"' /etc/v2ray/none.json
+cat>/etc/xray/$user-tls.json<<EOF
       {
       "v": "2",
       "ps": "${user}",
       "add": "${domain}",
       "port": "${tls}",
       "id": "${uuid}",
-      "aid": "0",
+      "aid": "2",
       "net": "ws",
       "path": "geo",
       "type": "none",
-      "host": "",
+      "host": "${bug}",
       "tls": "tls"
 }
 EOF
-cat>/etc/xray/v2ray-$user-nontls.json<<EOF
+cat>/etc/xray/$user-none.json<<EOF
       {
       "v": "2",
       "ps": "${user}",
       "add": "${bug}",
-      "port": "${nontls}",
+      "port": "${none}",
       "id": "${uuid}",
-      "aid": "0",
+      "aid": "2",
       "net": "ws",
       "path": "geo",
       "type": "none",
@@ -74,33 +63,38 @@ cat>/etc/xray/v2ray-$user-nontls.json<<EOF
 EOF
 vmess_base641=$( base64 -w 0 <<< $vmess_json1)
 vmess_base642=$( base64 -w 0 <<< $vmess_json2)
-xrayv2ray1="vmess://$(base64 -w 0 /etc/xray/v2ray-$user-tls.json)"
-xrayv2ray2="vmess://$(base64 -w 0 /etc/xray/v2ray-$user-nontls.json)"
-systemctl restart xray@v2ray-tls
-systemctl restart xray@v2ray-nontls
+vmesslink1="vmess://$(base64 -w 0 /etc/xray/$user-tls.json)"
+vmesslink2="vmess://$(base64 -w 0 /etc/xray/$user-none.json)"
+systemctl restart xray
+systemctl restart xray@none
 service cron restart
 clear
-echo -e "${cyan}=======================${off}"
-echo -e "${purple} ~> TRIAL XRAY / VMESS${off}"
-echo -e "${cyan}=======================${off}"
+echo -e "${cyan}=====================${off}"
+echo -e "${cyan}TRIAL XRAY /VMESS${off}"
+echo -e "${cyan}=====================${off}"
 echo -e " Remarks        : ${user}"
 echo -e " Bug            : ${bug}"
 echo -e " Domain         : ${domain}"
 echo -e " Port TLS       : ${tls}"
-echo -e " Port No TLS    : ${nontls}"
+echo -e " Port NON-TLS   : ${none}"
 echo -e " ID             : ${uuid}"
-echo -e " AlterID        : 0"
+echo -e " AlterID        : 2"
 echo -e " Security       : auto"
 echo -e " Network        : ws"
 echo -e " Path           : geo${off}"
-echo -e "${cyan}==================================${off}"
-echo -e "${purple}~> VMESS TLS : $off${xrayv2ray1}"
-echo -e "${cyan}==================================${off}"
-echo -e "${purple}~> VMESS NON-TLS : $off${xrayv2ray2}"
-echo -e "${cyan}==================================${off}"
+echo -e "${cyan}=====================${off}"
+echo -e "${purple}~> VMESS TLS${off}"
+echo -e ""
+echo -e "${vmesslink1}"
+echo -e ""
+echo -e "${cyan}====================${off}"
+echo -e "${purple}~> VMESS NON-TLS${off}"
+echo -e ""
+echo -e "${vmesslink2}"
+echo -e ""
+echo -e "${cyan}====================${off}"
 echo -e " ${green}Aktif Selama   : $masaaktif Hari"
-echo -e "${cyan}==================================${off}"
+echo -e "${cyan}====================${off}"
 echo -e ""
-echo -e "Script By GeoVPN" | lolcat
+echo -e "Script By Geo.NTB" | lolcat
 echo -e ""
-
