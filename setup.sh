b399d9fad78a2b74c8a077f6688dead2a84839f7
Muhammad Amin <96859296+geovpn@users.noreply.github.com>
2022-01-26 17:48:02 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+if [ "${EUID}" -ne 0 ]; then
+		echo "You need to run this script as root"
+		exit 1
+fi
+if [ "$(systemd-detect-virt)" == "openvz" ]; then
+		echo "OpenVZ is not supported"
+		exit 1
+fi
 # Link Hosting Kalian Untuk Ssh Vpn
 geovpn="raw.githubusercontent.com/geovpn/mantap/main/ssh"
 # Link Hosting Kalian Untuk Sstp
