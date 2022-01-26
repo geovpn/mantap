0b5bc2dcf28ab8eb091f7573eb3c6463fe809141
Muhammad Amin <96859296+geovpn@users.noreply.github.com>
2022-01-26 14:59:30 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -131,7 +131,35 @@ sudo hostnamectl set-hostname Setup-Geo-Project
 apt-get install figlet -y > /dev/null
 apt-get install ruby -y > /dev/null
 gem install lolcat
-
+clear
+cd
+red='\e[1;31m'
+green='\e[0;32m'
+yell='\e[1;33m'
+NC='\e[0m'
+curl -sS https://raw.githubusercontent.com/geovpn/perizinan/main/ascii-home
+echo "Setting UP"
+echo "Progress..."
+sleep 3
+echo ""
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+PERMISSION
+if [ "$res" = "Permission Accepted..." ]; then
+green "Permission Accepted.."
+else
+red "Permission Denied!"
+exit 0
+fi
+sleep 3
+echo -e "
+"
+date
+echo ""
+sleep 1
+echo -e "[ ${green}INFO${NC} ] Checking... "
+sleep 2
+sleep 1
 clear
 if [ -f "/etc/xray/domain" ]; then
 echo "Script Already Installed"
