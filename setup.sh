19e51db1604416a180ee292e02e885286b4bb4df
Muhammad Amin <96859296+geovpn@users.noreply.github.com>
2022-01-27 18:02:35 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -393,7 +393,7 @@ echo "   - Full Orders For Various Services" | tee -a log-install.txt
 echo "   - White Label" | tee -a log-install.txt
 echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
-echo "   - Dev/Main                : Muhammad Amin Anak Nusa Tenggara Barat"  | tee -a log-install.txt
+echo "   - Dev/Main                : Amin Anak Nusa Tenggara Barat"  | tee -a log-install.txt
 echo "   - Recode                  : Muhammad Amin" | tee -a log-install.txt
 echo "   - Telegram                : T.me/sampiiiiu"  | tee -a log-install.txt
 echo "   - Instagram               : @geo_gabuter"  | tee -a log-install.txt
