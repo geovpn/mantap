83647d42810acd8a74f50d570772710193dbe3bf
Muhammad Amin <96859296+geovpn@users.noreply.github.com>
2022-01-27 18:01:34 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -369,8 +369,9 @@ echo "   - XRAYS Vmess None TLS    : 80"  | tee -a log-install.txt
 echo "   - XRAYS Vless TLS         : 2083"  | tee -a log-install.txt
 echo "   - XRAYS Vless None TLS    : 8880"  | tee -a log-install.txt
 echo "   - XRAYS Trojan            : 2087"  | tee -a log-install.txt
-echo "   - Websocket TLS           : 443"  | tee -a log-install.txt
-echo "   - Websocket None TLS      : 2095"  | tee -a log-install.txt
+echo "   - Websocket SSL           : 443"  | tee -a log-install.txt
+echo "   - Websocket Dropbear      : 2052"  | tee -a log-install.txt
+echo "   - Websocket OpenSSH       : 2095"  | tee -a log-install.txt
 echo "   - Websocket Ovpn          : 2086"  | tee -a log-install.txt
 echo "   - OHP SSH                 : 8181"  | tee -a log-install.txt
 echo "   - OHP Dropbear            : 8282"  | tee -a log-install.txt
